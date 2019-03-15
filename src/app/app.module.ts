ff65c4150ae5cbd15464e2e791258a61acfa62af
Malkieg <malkieg970@gmail.com>
2019-03-15 11:02:52 -0400

    most recent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatNativeDateModule
+} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AssignmentsComponent } from './assignments/assignments.component';
+import { SubmittedDirective } from './shared/submitted.directive';
+import { UnsubmittedDirective } from './shared/unsubmitted.directive';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AssignmentDetailComponent } from './assignments/assignment-detail/assignment-detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AssignmentsComponent
+    AssignmentsComponent,
+    SubmittedDirective,
+    UnsubmittedDirective,
+    AssignmentDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/assignments/assignments.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Assignment} from './assignment.model';
+
+@Component({
+  selector: 'app-assignments',
+  templateUrl: './assignments.component.html',
+  styleUrls: ['./assignments.component.css']
+})
+export class AssignmentsComponent implements OnInit {
+  title = 'My Assignments Application';
+  enabled = false;
+  name: string;
+  dueDate: Date;
+
+  selectedAssignment: Assignment;
+  assignments: Assignment[] = [{
+    name: 'One',
+    dueDate: new Date('2018-01-01'),
+    submitted: true
+  },
+    {
+      name: 'Two',
+      dueDate: new Date('2019-01-01'),
+      submitted: false
+    }];
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout( () => {
+      this.enabled = true;
+    }, 2000);
+  }
+
+  onSubmit() {
+    const assignment = new Assignment();
+    assignment.name = this.name;
+    assignment.dueDate = this.dueDate;
+    assignment.submitted = false;
+    this.assignments.push(assignment);
+  }
+  setSelected(assignment: Assignment) {
+    this.selectedAssignment = assignment;
+  }
+
+}
